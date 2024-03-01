4f94f914de5511f11a4bc330cfdce00c7cdc116e
KLO1HC <long.k@vn.bosch.com>
2024-03-01 17:47:00 +0700

    Initialize commit

---
+++ FastIntegration.DaimlerClient/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FastIntegration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, This is the CLI for Fast Integrate Daimler Client!");
+            Console.ReadLine();
+        }
+    }
+}
